2ceb0394f22eaa2c5c0083fdff06cc5b68cc5790
Nikita Bibik <osbib2@gmail.com>
2019-11-18 21:14:35 +0300

    Implement http api for getting information about administration.

--- node/src/api.rs
+++ node/src/api.rs
@@ -1,4 +1,8 @@
-use core::{constant::BLOCKCHAIN_SERVICE_ID, model::Participant, schema::ElectionSchema};
+use core::{
+    constant::BLOCKCHAIN_SERVICE_ID,
+    model::{Administration, Participant},
+    schema::ElectionSchema,
+};
 use exonum::{
     api::{self, ServiceApiBuilder, ServiceApiState},
     blockchain::{self, BlockProof, TransactionMessage},
@@ -33,6 +37,31 @@ pub struct ParticipantHistory {
 
 #[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq)]
 pub struct ParticipantQuery {
+    /// Public key of the queried wallet.
+    pub pub_key: PublicKey,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AdministrationInfo {
+    pub block_proof: BlockProof,
+    pub administration_proof: AdministrationProof,
+    pub administration_history: Option<AdministrationHistory>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AdministrationProof {
+    pub to_table: MapProof<Hash, Hash>,
+    pub to_administration: MapProof<PublicKey, Administration>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AdministrationHistory {
+    pub proof: ListProof<Hash>,
+    pub transactions: Vec<TransactionMessage>,
+}
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq)]
+pub struct AdministrationQuery {
     /// Public key of the queried wallet.
     pub pub_key: PublicKey,
 }
@@ -89,9 +118,61 @@ impl PublicApi {
         })
     }
 
+    pub fn administration_info(
+        state: &ServiceApiState,
+        query: AdministrationQuery,
+    ) -> api::Result<AdministrationInfo> {
+        let snapshot = state.snapshot();
+
+        let general_schema = blockchain::Schema::new(&snapshot);
+        let election_schema = ElectionSchema::new(&snapshot);
+
+        let max_height = general_schema.block_hashes_by_height().len() - 1;
+
+        let block_proof = general_schema
+            .block_and_precommits(Height(max_height))
+            .unwrap();
+
+        let to_table: MapProof<Hash, Hash> =
+            general_schema.get_proof_to_service_table(BLOCKCHAIN_SERVICE_ID, 0);
+
+        let to_administration: MapProof<PublicKey, Administration> =
+            election_schema.administrations().get_proof(query.pub_key);
+
+        let administration_proof = AdministrationProof {
+            to_table,
+            to_administration,
+        };
+
+        let administration = election_schema.participant(&query.pub_key);
+
+        let administration_history = administration.map(|_| {
+            let history = election_schema.administration_history(&query.pub_key);
+            let proof = history.get_range_proof(0..history.len());
+            let explorer = BlockchainExplorer::new(state.blockchain());
+
+            let transactions = history
+                .iter()
+                .map(|record| explorer.transaction_without_proof(&record).unwrap())
+                .collect::<Vec<_>>();
+
+            AdministrationHistory {
+                proof,
+                transactions,
+            }
+        });
+
+        Ok(AdministrationInfo {
+            block_proof,
+            administration_proof,
+            administration_history,
+        })
+    }
+
     pub fn wire(builder: &mut ServiceApiBuilder) {
         builder
             .public_scope()
-            .endpoint("v1/participants/info", Self::participant_info);
+            .endpoint("v1/participants/info", Self::participant_info)
+            .endpoint("v1/administrations/info", Self::administration_info);
     }
 }
