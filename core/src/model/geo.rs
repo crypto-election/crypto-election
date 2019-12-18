5f886cedba8d2db1dfaafe4d2e99d98aafae4faa
integrer <osbib2@gmail.com>
2019-12-18 05:25:57 +0300

    Fixed geo data models conversion.

--- core/src/model/geo.rs
+++ core/src/model/geo.rs
@@ -1,65 +1,74 @@
-//#[derive(Serialize, Deserialize, ProtobufConvert, Clone)]
-//#[exonum(pb = "proto::PolygonDef")]
-//pub struct PolygonDef {
-//    exterior: LineStringDef,
-//    interiors: Vec<LineStringDef>,
-//}
-//
-//impl From<&PolygonDef> for Polygon<f64> {
-//    fn from(def: &PolygonDef) -> Self {
-//        Self::new(
-//            LineString::from(&def.exterior),
-//            def.interiors.iter().map(LineString::<f64>::from).collect(),
-//        )
-//    }
-//}
-//
-//impl From<&Polygon<f64>> for PolygonDef {
-//    fn from(polygon: &Polygon<f64>) -> Self {
-//        let (exterior, interiors) = polygon.clone().into_inner();
-//        PolygonDef {
-//            exterior: LineStringDef::from(&exterior),
-//            interiors: interiors.iter().map(LineStringDef::from).collect(),
-//        }
-//    }
-//}
-//
-//#[derive(Serialize, Deserialize, ProtobufConvert, Clone)]
-//#[exonum(pb = "proto::LineStringDef")]
-//struct LineStringDef {
-//    pub items: Vec<CoordinateDef>,
-//}
-//
-//impl From<&LineString<f64>> for LineStringDef {
-//    fn from(l_str: &LineString<f64>) -> Self {
-//        LineStringDef {
-//            items: l_str
-//                .0
-//                .iter()
-//                .map(|coord| CoordinateDef {
-//                    x: coord.x,
-//                    y: coord.y,
-//                })
-//                .collect(),
-//        }
-//    }
-//}
-//
-//impl From<&LineStringDef> for LineString<f64> {
-//    fn from(def: &LineStringDef) -> Self {
-//        def.items.iter().map(Coordinate::<f64>::from).collect()
-//    }
-//}
-//
-//#[derive(Serialize, Deserialize, ProtobufConvert, Copy, Clone)]
-//#[exonum(pb = "proto::CoordinateDef")]
-//struct CoordinateDef {
-//    pub x: f64,
-//    pub y: f64,
-//}
-//
-//impl From<&CoordinateDef> for Coordinate<f64> {
-//    fn from(def: &CoordinateDef) -> Self {
-//        Coordinate { x: def.x, y: def.y }
-//    }
-//}
+use serde::{Serialize, Deserialize};
+use geo;
+
+use crate::proto;
+
+
+#[derive(Serialize, Deserialize, ProtobufConvert, Clone)]
+#[exonum(pb = "proto::Polygon")]
+pub struct Polygon {
+   exterior: LineString,
+   interiors: Vec<LineString>,
+}
+
+#[derive(Serialize, Deserialize, ProtobufConvert, Clone)]
+#[exonum(pb = "proto::LineString")]
+struct LineString {
+   pub items: Vec<Coordinate>,
+}
+
+#[derive(Serialize, Deserialize, ProtobufConvert, Copy, Clone)]
+#[exonum(pb = "proto::Coordinate")]
+struct Coordinate {
+   pub x: f64,
+   pub y: f64,
+}
+
+impl From<geo::Polygon<f64>> for Polygon {
+   fn from(polygon: geo::Polygon<f64>) -> Self {
+       let (exterior, interiors) = polygon.into_inner();
+       Polygon {
+           exterior: exterior.into(),
+           interiors: interiors.into_iter().map(Into::into).collect(),
+       }
+   }
+}
+
+impl From<Polygon> for geo::Polygon<f64> {
+   fn from(def: Polygon) -> Self {
+       Self::new(
+           def.exterior.into(),
+           def.interiors.into_iter().map(Into::into).collect(),
+       )
+   }
+}
+
+impl From<geo::LineString<f64>> for LineString {
+   fn from(l_str: geo::LineString<f64>) -> Self {
+       LineString {
+           items: l_str
+               .0
+               .into_iter()
+               .map(Into::into)
+               .collect(),
+       }
+   }
+}
+
+impl From<LineString> for geo::LineString<f64> {
+   fn from(l_str: LineString) -> Self {
+       l_str.items.into_iter().map(Into::<geo::Coordinate<f64>>::into).collect()
+   }
+}
+
+impl From<geo::Coordinate<f64>> for Coordinate {
+   fn from(coord: geo::Coordinate<f64>) -> Self {
+       Coordinate { x: coord.x, y: coord.y }
+   }
+}
+
+impl From<Coordinate> for geo::Coordinate<f64> {
+   fn from(coord: Coordinate) -> Self {
+       geo::Coordinate { x: coord.x, y: coord.y }
+   }
+}
