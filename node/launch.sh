cf937db81764c09814482e5ab1c8f9b8068cc67b
Nikita Bibik <osbib2@gmai.com>
2020-02-12 22:35:41 +0300

    Node startup optimization.

--- node/launch.sh
+++ node/launch.sh
@@ -4,19 +4,25 @@ node_count=4
 start_peer_port=6331
 start_public_port=8000
 
-crypto-election-node generate-template common.toml --validators-count $node_count
+if [ ! -f common.toml ]; then
+  crypto-election-node generate-template common.toml --validators-count $node_count
+fi
 
 for i in $(seq 0 $((node_count - 1)))
 do
-  peer_port=$((start_peer_port + i))
-  crypto-election-node generate-config common.toml $((i + 1)) --peer-address 127.0.0.1:${peer_port} -n
+  if [ ! -f $((i + 1))/sec.toml ] && [ ! -f $((i + 1))/pub.toml ]; then
+    peer_port=$((start_peer_port + i))
+    crypto-election-node generate-config common.toml $((i + 1)) --peer-address 127.0.0.1:${peer_port} -n
+  fi
 done
 
 for i in $(seq 0 $((node_count - 1)))
 do
-  public_port=$((start_public_port + i))
-  private_port=$((public_port + node_count))
-  crypto-election-node finalize --public-api-address 0.0.0.0:${public_port} --private-api-address 0.0.0.0:${private_port} $((i + 1))/sec.toml $((i + 1))/node.toml --public-configs {1,2,3,4}/pub.toml
+  if [ ! -f $((i + 1))/node.toml ]; then
+    public_port=$((start_public_port + i))
+    private_port=$((public_port + node_count))
+    crypto-election-node finalize --public-api-address 0.0.0.0:${public_port} --private-api-address 0.0.0.0:${private_port} $((i + 1))/sec.toml $((i + 1))/node.toml --public-configs {1,2,3,4}/pub.toml
+  fi
 done
 
 for i in $(seq 0 $((node_count - 1)))
