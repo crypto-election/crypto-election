a08cd1f82be109077a024d4879fb202f3da08ece
integrer <osbib2@gmail.com>
2020-04-26 20:56:20 +0300

    Add user kind field

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,23 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-const KEY = 'cryptocurrency'
-const keyPair = JSON.parse(localStorage.getItem(KEY))
+const KEY_KEYPAIR = 'cryptoelection-keypair'
+const keyPair = JSON.parse(localStorage.getItem(KEY_KEYPAIR))
+const KEY_USER_KIND = 'cryptoelection-userkind'
+const userKind = JSON.parse(localStorage.getItem(KEY_USER_KIND))
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    keyPair: keyPair
+    keyPair: keyPair,
+    userKind: userKind
   },
   mutations: {
     login: (state, keyPair) => {
-      localStorage.setItem(KEY, JSON.stringify(keyPair))
+      localStorage.setItem(KEY_KEYPAIR, JSON.stringify(keyPair))
       state.keyPair = keyPair
+      localStorage.setItem(KEY_USER_KIND, JSON.stringify(userKind))
+      state.userKind = userKind
     },
     logout: state => {
-      localStorage.removeItem(KEY)
+      localStorage.removeItem(KEY_KEYPAIR)
       state.keyPair = null
+      localStorage.removeItem(KEY_USER_KIND)
+      state.userKind = null
     }
   }
 })
