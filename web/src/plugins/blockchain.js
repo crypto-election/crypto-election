0bca3918f832632198adcfbd82e490d58345b961
integrer <osbib2@gmail.com>
2020-04-26 20:55:37 +0300

    split code of getAdministration and getParticipant into subfunctions. Add reusage of code.

--- web/src/plugins/blockchain.js
+++ web/src/plugins/blockchain.js
@@ -17,7 +17,9 @@ const TX_SUBMIT_LOCATION_ID = 4;
 const TABLE_INDEX = 0
 
 // MyCode
-let Participant = Exonum.newType(proto.crypto_election.core.Participant);
+const Participant = Exonum.newType(proto.crypto_election.core.Participant);
+const Administration = Exonum.newType(proto.crypto_election.core.Administration);
+const PublicKey = Exonum.MapProof.rawKey(Exonum.PublicKey)
 //const Wallet = Exonum.newType(proto.exonum.examples.cryptocurrency_advanced.Wallet)
 
 //MyCode
@@ -87,33 +89,154 @@ function deserializeTx (transaction) {
   return { name: 'initialTx' }
 }
 
+async function getConsensusConfig() { return await axios.get('/api/services/supervisor/consensus-config') }
+
+async function getValidatorsConsensusKeys() {
+  return (await getConsensusConfig()).data.validator_keys
+    .map(validator => validator.consensus_key)
+}
+
 /**
- * Data for {@link CreateAdninistrationTransaction}
+ * @template K
+ * Queries from validator proofed info for some entity
+ * @param {string} entityName 
+ * @param {K} key 
+ * @returns {ProofedInfo<K, *>}
+ */
+async function getEntityProofedInfo(entityName, key) {
+  const path = `${SERVICE_PUBLIC_API_PATH}/${entityName}/info?key=${key}`;
+  return (await axios.get(path)).data
+}
+
+function getTableRootHash(object_proof, block_proof, tableName) {
+  // verify table timestamps in the root tree
+  return Exonum.verifyTable(
+      object_proof.to_table,
+      block_proof.block.state_hash,
+      `${SERVICE_NAME}.${tableName}`
+    )
+}
+
+function requireVerifiedEntity(object_proof, tableRootHash, entityName, keyType, valueType, key) {
+  // find wallet in the tree of all wallets
+  const objectProof = new Exonum.MapProof(object_proof.to_object, keyType, valueType)
+  if (objectProof.merkleRoot !== tableRootHash) throw new Error(entityName + ' proof is corrupted')            
+  const entity = objectProof.entries.get(key)
+  if (typeof entity == 'undefined') throw new Error(entityName + ' not found')
+  return entity
+}
+
+function verifyTransactionProof(verifiedTransactions, historyHash) {
+  const hexHistoryHash = Exonum.uint8ArrayToHexadecimal(new Uint8Array(historyHash.data))
+  if (verifiedTransactions.merkleRoot !== hexHistoryHash)
+    throw new Error('Transactions proof is corrupted')
+}
+
+function verifyTransactionIndexes(verifiedTransactions) {
+  const isIndexesValid = verifiedTransactions.entries
+    .every(({ index }, i) => i === index)
+  if (!isIndexesValid) throw new Error('Invalid transaction indexes in the proof')
+}
+
+function verifyTransactionHashes(transactions, verifiedTransactions) {
+  const isHashesCorrect = transactions
+    .every(({ hash }, i) => verifiedTransactions.entries[i].value === hash)
+  if (!isHashesCorrect) throw new Error('Transaction hash mismatch')
+}
+
+function getVerifiedTransactions(history, historyHash) {
+  const verifiedTransactions = new Exonum.ListProof(history.proof, Exonum.Hash)
+  verifyTransactionProof(verifiedTransactions, historyHash)
+  verifyTransactionIndexes(verifiedTransactions)    
+  // deserialize transactions
+  const transactions = history.transactions.map(deserializeTx)    
+  verifyTransactionHashes(transactions, verifiedTransactions)    
+  return transactions;
+}
+
+/**
+ * Data for {@link CreateAdministrationTransaction}
  * @typedef {Object} CreateAdministrationTransactionData
  * @prop {string} name                - Administration`s name
- * @prop {ArrayBuffer?} principal_key - Administration`s email
- * @prop {Object} area                - Administration`s phone number
+ * @prop {ArrayBuffer?} principal_key - Administration`s principal administration key
+ * @prop {Polygon} area               - Administration`s phone number
+ */
+
+ /**
+  * @template K Key type
+  * @template V Value type
+  * @typedef {Object} ProofedInfo
+  * @prop {*} block_proof
+  * @prop {Proof<K, V>} object_proof
+  * @prop {History?} history
+  */
+
+ /**
+  * @template K Key type
+  * @template V Value type
+  * @typedef {Object} Proof
+  * @prop {MapProof<string, Hash>} to_table
+  * @prop {MapProof<K, V>} to_object
+  * @prop {History?} history
+  */
+
+  /**
+  * @template K Key type
+  * @template V Value type
+  * @typedef {Object} MapProof
+  * @prop {OptionalEntry<K, V>[]} entries
+  * @prop {MapProofEntry[]} proof
+  */
+
+ /**
+   * @typedef {Object} History
+   * @prop {*} proof
+   * @prop {*[]} transactions
+   */
+
+/**
+ * Area polygon
+ * ## Example
+ * ```javascript
+ * let polygon = {
+ *   exterior: [[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0]],
+ *   interiors: [
+ *     [[0.1, 0.1], [0.4, 0.1], [0.4, 0.1], [0.1, 0.4]],
+ *     [[0.6, 0.6], [0.9, 0.6], [0.9, 0.9], [0.6, 0.9]],
+ *   ],
+ * }
+ * ```
+ * @typedef {Object} Polygon
+ * @prop {number[][]} exterior       - External boundary 
+ * @prop {number[][][]} interiors    - Internal boundaries
  */
 
 /**
  * Data for {@link CreateParticipantTransaction}
  * @typedef {Object} CreateParticipantTransactionData
- * @prop {string} name            - Participant`s name
- * @prop {string} email           - Participant`s email
- * @prop {string} phone_number    - Participant`s phone number
- * @prop {ArrayBuffer?} residence - Participant`s residence
- * @prop {string} pass_code       - Participant`s passport code
+ * @prop {string} name                - Participant`s name
+ * @prop {string} email               - Participant`s email
+ * @prop {string} phone_number        - Participant`s phone number
+ * @prop {ArrayBuffer?} residence     - Participant`s residence administration key
+ * @prop {string} pass_code           - Participant`s passport code
  */
 
 
 /**
  * Data for {@link IssueElectionTransaction}
  * @typedef {Object} IssueElectionTransactionData
- * @prop {string} name            - Elections name
- * @prop {date} start_date        - Elections start_date
- * @prop {date} finish_date       - Elections finish_date
- * @prop {Array} options          - Elections options
+ * @prop {string} name                - Election`s name
+ * @prop {date} start_date            - Election`s start_date
+ * @prop {date} finish_date           - Election`s finish_date
+ * @prop {Array} options              - Election`s options
  */
+
+ /**
+  * Pair of secret and public keys
+  * @typedef {Object} KeyPair
+  * @prop {string} publicKey          - Public key hexadecimal representation
+  * @prop {string} secretKey          - Secret key hexadecimal representation
+  */
 
 
 module.exports = {
@@ -126,26 +249,12 @@ module.exports = {
       generateSeed() {
         return Exonum.randomUint64()
       },
-      
-
-      /**
-       * Sends {@link CreateAdministrationTransaction}.
-       * @param {*} keyPair 
-       * @param {CreateAdministrationTransactionData} data - Transcation data
-       */
 
       /**
        * Sends {@link CreateParticipantTransaction}.
-       * @param {*} keyPair 
+       * @param {KeyPair} keyPair 
        * @param {CreateParticipantTransactionData} data - Transcation data
        */
-      
-       /**
-       * Sends {@link IssueElectionTransaction}.
-       * @param {*} keyPair 
-       * @param {IssueElectionTransactionData} data - Transcation data
-       */
-
       createParticipant(keyPair, data) {
         // Describe transaction
         const transaction = CreateParticipantTransaction.create(data, keyPair).serialize()
@@ -154,6 +263,11 @@ module.exports = {
         return Exonum.send(TRANSACTION_URL, transaction)
       },
 
+      /**
+       * Sends {@link CreateAdministrationTransaction}.
+       * @param {KeyPair} keyPair 
+       * @param {CreateAdministrationTransactionData} data - Transcation data
+       */
       createAdministration(keyPair, data) {
         // Describe transaction
         const transaction = CreateAdministrationTransaction.create(data, keyPair).serialize()
@@ -162,6 +276,11 @@ module.exports = {
         return Exonum.send(TRANSACTION_URL, transaction)
       },
 
+       /**
+       * Sends {@link IssueElectionTransaction}.
+       * @param {KeyPair} keyPair 
+       * @param {IssueElectionTransactionData} data - Transcation data
+       */
       createNewPoll(keyPair, data) {
         // Describe transaction
         const transaction = IssueElectionTransaction.create(data, keyPair).serialize()
@@ -169,6 +288,7 @@ module.exports = {
         // Send transaction into blockchain
         return Exonum.send(TRANSACTION_URL, transaction)
       },
+      
       addFunds(keyPair, amountToAdd, seed) {
         // Describe transaction
         const transaction = new IssueTransaction(keyPair.publicKey)
@@ -198,122 +318,70 @@ module.exports = {
         return transaction.send(TRANSACTION_URL, data, keyPair.secretKey)
       },
       //my big code
-      getAdministration(publicKey) {
-        return axios.get('/api/services/supervisor/consensus-config').then(response => {
-          // actual list of public keys of validators
-          const validators = response.data.validator_keys.map(validator => validator.consensus_key)
+      async getAdministration(publicKey) {
+        const validators = await getValidatorsConsensusKeys()
+        const { block_proof, object_proof, history } =
+          await getEntityProofedInfo('administrations', publicKey)
 
-          return axios.get(`${SERVICE_PUBLIC_API_PATH}/administrations/info?key=${publicKey}`)
-              .then(response => response.data)
-              .then(({ block_proof, object_proof, history }) => {
-                Exonum.verifyBlock(block_proof, validators)
-                // verify table timestamps in the root tree
-                const tableRootHash = Exonum.verifyTable(
-                    object_proof.to_table,
-                    block_proof.block.state_hash,
-                    `${SERVICE_NAME}.administrations`)
+        Exonum.verifyBlock(block_proof, validators)
+        // verify table timestamps in the root tree
+        const tableRootHash = getTableRootHash(object_proof, block_proof, 'administrations')
+        
+        const administration = requireVerifiedEntity(
+          object_proof,
+          tableRootHash,
+          'Administration',
+          PublicKey,
+          Administration,
+          Exonum.publicKeyToAddress(publicKey)
+        )
 
-                // find wallet in the tree of all wallets
-                const administrationProof = new Exonum.MapProof(
-                    object_proof.to_object,
-                    Exonum.MapProof.rawKey(Exonum.PublicKey),
-                    Administration)
-                if (administrationProof.merkleRoot !== tableRootHash)
-                  throw new Error('Administration proof is corrupted')
-
-                const administration = administrationProof.entries.get(Exonum.publicKeyToAddress(publicKey))
-                if (typeof administration == 'undefined') throw new Error('Administration not found')
-
-                const verifiedTransactions = new Exonum.ListProof(history.proof, Exonum.Hash)
-                const hexHistoryHash = Exonum.uint8ArrayToHexadecimal(new Uint8Array(administration.history_hash.data))
-                if (verifiedTransactions.merkleRoot !== hexHistoryHash) throw new Error('Transactions proof is corrupted')
-
-                const validIndexes = verifiedTransactions
-                    .entries
-                    .every(({ index }, i) => i === index)
-                if (!validIndexes) throw new Error('Invalid transaction indexes in the proof')
-
-                // deserialize transactions
-                const transactions = history.transactions.map(deserializeTx)
-
-                const correctHashes = transactions.every(({ hash }, i) => verifiedTransactions.entries[i].value === hash)
-                if (!correctHashes) throw new Error('Transaction hash mismatch')
-
-                return {
-                  block: block_proof.block,
-                  administrations,
-                  transactions
-                }
-            })
-        })
+        return {
+          block: block_proof.block,
+          administration,
+          transactions: getVerifiedTransactions(history, administration.history_hash)
+        }
       },
       //end my big code
-      getParticipant(publicKey) {
-        return axios.get('/api/services/supervisor/consensus-config').then(response => {
-          // actual list of public keys of validators
-          const validators = response.data.validator_keys.map(validator => validator.consensus_key)
+      async getParticipant(publicKey) {
+        // actual list of public keys of validators
+        const validators = await getValidatorsConsensusKeys()
+        const { block_proof, object_proof, history } =
+          await getEntityProofedInfo('participants', publicKey)
+        
+        Exonum.verifyBlock(block_proof, validators)
 
-          return axios.get(`${SERVICE_PUBLIC_API_PATH}/participants/info?key=${publicKey}`)
-              .then(response => response.data)
-              .then(({ block_proof, object_proof, history }) => {
-                Exonum.verifyBlock(block_proof, validators)
-                // verify table timestamps in the root tree
-                const tableRootHash = Exonum.verifyTable(
-                    object_proof.to_table,
-                    block_proof.block.state_hash,
-                    `${SERVICE_NAME}.participants`)
+        const tableRootHash = getTableRootHash(object_proof, block_proof, 'participants')
 
-                // find wallet in the tree of all wallets
-                const participantProof = new Exonum.MapProof(
-                    object_proof.to_object,
-                    Exonum.MapProof.rawKey(Exonum.PublicKey),
-                    Participant)
-                if (participantProof.merkleRoot !== tableRootHash)
-                  throw new Error('Participant proof is corrupted')
+        const participant = requireVerifiedEntity(
+          object_proof,
+          tableRootHash,
+          'Participant',
+          PublicKey,
+          Participant,
+          Exonum.publicKeyToAddress(publicKey)
+        )
 
-                const participant = participantProof.entries.get(Exonum.publicKeyToAddress(publicKey))
-                if (typeof participant == 'undefined') throw new Error('Participant not found')
-
-                const verifiedTransactions = new Exonum.ListProof(history.proof, Exonum.Hash)
-                const hexHistoryHash = Exonum.uint8ArrayToHexadecimal(new Uint8Array(participant.history_hash.data))
-                if (verifiedTransactions.merkleRoot !== hexHistoryHash) throw new Error('Transactions proof is corrupted')
-
-                const validIndexes = verifiedTransactions
-                    .entries
-                    .every(({ index }, i) => i === index)
-                if (!validIndexes) throw new Error('Invalid transaction indexes in the proof')
-
-                // deserialize transactions
-                const transactions = history.transactions.map(deserializeTx)
-
-                const correctHashes = transactions.every(({ hash }, i) => verifiedTransactions.entries[i].value === hash)
-                if (!correctHashes) throw new Error('Transaction hash mismatch')
-
-                return {
-                  block: block_proof.block,
-                  participant,
-                  transactions
-                }
-            })
-        })
+        return {
+          block: block_proof.block,
+          participant,
+          transactions: getVerifiedTransactions(history, participant.history_hash)
+        }
       },
 
-      getBlocks(latest) {
+      async getBlocks(latest) {
         const suffix = !isNaN(latest) ? '&latest=' + latest : ''
-        return axios.get(`/api/explorer/v1/blocks?count=${PER_PAGE}${suffix}`).then(response => response.data)
+        return (await axios.get(`/api/explorer/v1/blocks?count=${PER_PAGE}${suffix}`)).data
       },
 
-      getBlock(height) {
-        return axios.get(`/api/explorer/v1/block?height=${height}`).then(response => response.data)
+      async getBlock(height) {
+        return (await axios.get(`/api/explorer/v1/block?height=${height}`)).data
       },
 
-      getTransaction(hash) {
-        return axios.get(`/api/explorer/v1/transactions?hash=${hash}`)
-            .then(response => response.data)
-            .then(data => {
-              data.content = deserializeTx(data.message)
-              return data
-            })
+      async getTransaction(hash) {
+        const { data } = await axios.get(`/api/explorer/v1/transactions?hash=${hash}`)
+        data.content = deserializeTx(data.message)
+        return data
       }
     }
   }
